d2f9de117e2844a9f153634f64ca26f0fe8e9be7
sensucharita15 <sensucharita15@gmail.com>
2023-04-27 10:03:55 -0400

    NameGusessingMain

---
+++ src/StudentNameGuessing/StudentGameNMain.java
@@ -0,0 +1,14 @@
+package StudentNameGuessing;
+
+public class StudentGameNMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String filePath="C:\\Users\\santa\\OneDrive\\Desktop\\sucharita\\STUDENTNAME.txt";
+		StudentNameGuessingGameN game=new StudentNameGuessingGameN(filePath);
+		game.displayRandomName();
+game.Play();
+	}
+
+}
