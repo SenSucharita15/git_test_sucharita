7f6b42678aefc525d665d95c4726889c7d2d960f
sensucharita15 <sensucharita15@gmail.com>
2023-05-02 10:19:01 -0400

    Inheritance modified

--- src/assignmentInheritance/ATM.java
+++ src/assignmentInheritance/ATM.java
@@ -5,7 +5,7 @@ import java.util.*;
 
 public class ATM extends BankOperation {
 	private int pin;
-	private int count;
+	// private int count;
 
 	public ATM(double balance, int pin) {
 		super(balance);
@@ -17,63 +17,74 @@ public class ATM extends BankOperation {
 	public void performTransaction() {
 		Scanner sc = new Scanner(System.in);
 		boolean flag = false;
-		
-		System.out.println("Enter the pin");
-		int enteredPin = sc.nextInt();
-		
-		if (enteredPin == pin) {
-			System.out.println(" You have logged in successfully\n");
-			// System.out.println("1. Deposit\n 2. withdraw \n 3. View Balance \n 4. exit");
-		
-			while ( !flag ) {
-				
-				
-	System.out.println("\n 1. Deposit\n 2. withdraw \n 3. View Balance \n 4. exit\n ");
-				int choice = sc.nextInt();
-			//	System.out.println(" want to continue Enter your choice");
-				switch (choice) {
 
-				case 1:
-					System.out.println("Enter the amount to be deposited");
-					double depositAmount = sc.nextDouble();
-					deposit(depositAmount);
-					System.out.println(" \n want to continue Enter your choice");
-					break;
+		// System.out.println("Enter the pin");
+		// int enteredPin = sc.nextInt();
 
-				case 2:
-					System.out.println("Enter the amount to withdraw");
-					double withdrawAmount = sc.nextDouble();
-					withdraw(withdrawAmount);
-					System.out.println(" \n want to continue Enter your choice");
-					break;
+		int count = 0;
+		// if (enteredPin == pin && count<3) {
+		while (count < 3) {
+			System.out.println("Enter the pin");
+			int enteredPin = sc.nextInt();
+			if (enteredPin == pin) {
 
-				case 3:
-					viewBalance();
-					System.out.println(" \n want to continue Enter your choice");
-					break;
-
-				default:
-					System.out.println("Invalid choice.Enter a valid choice from the above option");
-					System.out.println(" \n want to continue Enter your choice");
-					break;
-
-				case 4:
-					flag = true;
-					break;
-				}
-				
-			
+				System.out.println(" You have logged in successfully\n");
+				break;
+			} else {
+				count++;
+				System.out.println("Invalid Pin Try again.Attempts remaining  :" + (3 - count));
 			}
-			
-	}
-
-		else {
-			System.out.println("Invalid pin, Try Again :");
-			
 		}
-			
-			
-		System.out.println("Thank You ");
+		if (count == 3) {
+			System.out.println("Thank You ");
+			System.out.println(" Account Locked");
+			return;
+		
+			//break;
+		}
 
+		// System.out.println("1. Deposit\n 2. withdraw \n 3. View Balance \n 4. exit");
+
+		while (!flag) {
+
+			System.out.println("\n 1. Deposit\n 2. withdraw \n 3. View Balance \n 4. exit\n ");
+			int choice = sc.nextInt();
+			// System.out.println(" want to continue Enter your choice");
+			switch (choice) {
+
+			case 1:
+				System.out.println("Enter the amount to be deposited");
+				double depositAmount = sc.nextDouble();
+				deposit(depositAmount);
+				System.out.println(" \n want to continue Enter your choice");
+				break;
+
+			case 2:
+				System.out.println("Enter the amount to withdraw");
+				double withdrawAmount = sc.nextDouble();
+				withdraw(withdrawAmount);
+				System.out.println(" \n want to continue Enter your choice");
+				break;
+
+			case 3:
+				viewBalance();
+				System.out.println(" \n want to continue Enter your choice");
+				break;
+
+			default:
+				System.out.println("Invalid choice.Enter a valid choice from the above option");
+				System.out.println(" \n want to continue Enter your choice");
+				break;
+
+			case 4:
+				flag = true;
+				break;
+			}
+
+		}
+
+
+		System.out.println("Thank You ");
 	}
+
 }
